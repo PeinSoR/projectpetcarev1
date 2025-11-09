b5c4a9f6f89b5ccd07277fa947dc2c91455c4bc8
SokuGreyrat <moiseslopezchavez8@gmail.com>
2025-11-08 20:50:56 -0600

    Ventanas

---
+++ petcaref/src/app/pages/adopciones/adopciones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adopciones',
+  imports: [],
+  templateUrl: './adopciones.html',
+  styleUrl: './adopciones.css',
+})
+export class Adopciones {
+
+}
---
+++ petcaref/src/app/pages/red-vecinal/red-vecinal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-red-vecinal',
+  imports: [],
+  templateUrl: './red-vecinal.html',
+  styleUrl: './red-vecinal.css',
+})
+export class RedVecinal {
+
+}
