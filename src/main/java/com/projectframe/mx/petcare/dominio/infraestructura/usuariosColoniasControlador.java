cd8afcbf3dc83ac709337b04bc080b302e9d8cc8
Alejandro Huesca <91446260+PeinSoR@users.noreply.github.com>
2025-12-09 23:19:53 -0600

    Fixes de validaciones de colonias

--- src/main/java/com/projectframe/mx/petcare/dominio/infraestructura/usuariosColoniasControlador.java
+++ src/main/java/com/projectframe/mx/petcare/dominio/infraestructura/usuariosColoniasControlador.java
@@ -1,11 +1,14 @@
 package com.projectframe.mx.petcare.dominio.infraestructura;
 
+import com.projectframe.mx.petcare.dominio.aplicacion.coloniasServicio;
 import com.projectframe.mx.petcare.dominio.aplicacion.usuariosColoniasServicio;
+import com.projectframe.mx.petcare.dominio.entidades.colonias;
 import com.projectframe.mx.petcare.dominio.entidades.usuariosColonias;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,6 +17,9 @@ import java.util.List;
 public class usuariosColoniasControlador {
     @Autowired
     private usuariosColoniasServicio  usuariosColoniasServicio;
+
+    @Autowired
+    private coloniasServicio coloniasServicio;
 
     @GetMapping("/allusuarios-colonias")
     @ResponseStatus(HttpStatus.OK)
@@ -30,6 +36,10 @@ public class usuariosColoniasControlador {
     @PostMapping("/create-usuarios-colonias")
     @ResponseStatus(HttpStatus.OK)
     public usuariosColonias guardarUsuariosColonias(@RequestBody usuariosColonias usuariosColonias){
+        colonias col = coloniasServicio.obtenerColoniaPorId(usuariosColonias.getColoniaId());
+        if (col == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La colonia no existe");
+        }
         return usuariosColoniasServicio.guardarUsuariosColonias(usuariosColonias);
     }
 
