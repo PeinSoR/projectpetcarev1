56acf481b14c463dde337f49ad43ab1a2bb50380
Alejandro Huesca <91446260+PeinSoR@users.noreply.github.com>
2025-12-09 22:36:37 -0600

    Fixes de id en update y delete

--- src/main/java/com/projectframe/mx/petcare/dominio/aplicacion/impl/commentsServicioImpl.java
+++ src/main/java/com/projectframe/mx/petcare/dominio/aplicacion/impl/commentsServicioImpl.java
@@ -4,6 +4,7 @@ import com.projectframe.mx.petcare.dominio.aplicacion.commentsServicio;
 import com.projectframe.mx.petcare.dominio.entidades.comments;
 import com.projectframe.mx.petcare.dominio.repositorios.commentsRepositorio;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Service
 @Slf4j
 public class commentsServicioImpl implements commentsServicio {
+    @Autowired
     private commentsRepositorio commRepo;
 
     @Override
