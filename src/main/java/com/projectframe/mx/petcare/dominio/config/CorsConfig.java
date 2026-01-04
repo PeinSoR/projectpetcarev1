dae2f1aca7978efaa2298cca9ee717443ce1f19d
Alejandro Huesca <91446260+PeinSoR@users.noreply.github.com>
2026-01-04 01:57:46 -0600

    implementacion de cors

---
+++ src/main/java/com/projectframe/mx/petcare/dominio/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.projectframe.mx.petcare.dominio.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "https://petcare-qsvz.onrender.com"
+                )
+                .allowedMethods(
+                        "GET", "POST", "PUT", "DELETE", "OPTIONS"
+                )
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
